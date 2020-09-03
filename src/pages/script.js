1d5ce230c06e0085310f11c44f12a3cb1e635c94
Ernesto Vera Celiz <evcvera@gmail.com>
2020-09-03 18:17:51 -0300

    casi actividad 3

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,11 +1,43 @@
 let lista = [];
 let cadena = "";
+let contador = 4;
+let bandera = false;
+let adiviniza = "";
+const adiviniza1 = "Lorem ipsum1 ";
+const adiviniza2 = "Lorem ipsum2 ";
+const adiviniza3 = "Lorem ipsum3 ";
+const adivinizaAcertada = "Felicidades";
 
-function mostrar_nombre() {
+
+function adivinar() {
     event.preventDefault();
     let elemento = document.getElementById('txtNombre');
-    let nombre = elemento.value;
-    cadena += "<li>" + nombre + "</li>";
+    let cadena = elemento.value;
+    /**cadena += "<p>" + nombre + "<x/p>";
     console.log(cadena);
-    document.getElementById('lista').innerHTML = cadena;
+    document.getElementById('lista').innerHTML = cadena;**/
+    if (contador > 0 && bandera != true) {
+        if (cadena === "chocolate") {
+            bandera = true;
+            adiviniza += "<p>" + adivinizaAcertada + "</p>"
+            document.getElementById('lista').innerHTML = adiviniza;
+        }
+        else {
+            contador -= 1;
+            console.log(contador)
+            if (contador === 3) {
+                adiviniza += "<p>" + adiviniza1 + "</p>"
+                document.getElementById('lista').innerHTML = adiviniza;
+            }
+            else if (contador === 2) {
+                adiviniza += "<p>" + adiviniza2 + "</p>"
+                document.getElementById('lista').innerHTML = adiviniza;
+            }
+            else if (contador === 1) {
+                adiviniza += "<p>" + adiviniza3 + "</p>"
+                document.getElementById('lista').innerHTML = adiviniza;
+            }
+        }
+    }
+
 }
